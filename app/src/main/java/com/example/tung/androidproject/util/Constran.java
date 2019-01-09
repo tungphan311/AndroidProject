b80a8561adcac0ad20172ccb39bb14a03450a798
Phan Thanh Tung <a1midas@outlook.com>
2019-01-10 02:32:18 +0700

    add list cac loai san pham activity

--- app/src/main/java/com/example/tung/androidproject/util/Constran.java
+++ app/src/main/java/com/example/tung/androidproject/util/Constran.java
@@ -9,6 +9,8 @@ public class Constran {
     public static final int RESULT_CODE_ME_DANGNHAP = 152;
     public static final String KEY_ME_DANGNHAP = "me_dangnhap";
 
+    public static String connectionErrorMessage = "Vui lòng kiểm tra kết nối Internet";
+
     public static String localhost = "10.0.133.201:8080";
     public static String API_URL = "http://" + localhost + "/server/";
     public static String getLoaiSP_URL = "http://" + localhost + "/server/getloaisanpham.php";
